854c38733ae87e7df2528a36b82436b0ad01be36
Unknown <47182802+UnknownOre@users.noreply.github.com>
2019-03-09 23:02:50 +0200

    Create EventListener.php

---
+++ src/UnknownOre/Pen15/EventListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace UnknownOre\Pen15;
+
+use pocketmine\event\inventory\CraftItemEvent;
+use pocketmine\event\Listener;
+
+Class EventListener implements Listener{
+    
+    /** @var Pen15 */
+    private $plugin;
+    
+    /**
+     * EventListener constructor.
+     * @param Pen15 $plugin
+     */
+	public function __construct(Pen15 $plugin){
+		$this->plugin = $plugin;
+	}
+    
+    /**
+	 * @param CraftItemEvent $event
+	 */
+    public function onCraft(CraftItemEvent $event){
+        $recipe = $event->getRecipe();
+        if($recipe == $this->plugin->recipe){
+            $event->getPlayer()->sendMessage('hello beautiful');
+        }
+    }
+}
